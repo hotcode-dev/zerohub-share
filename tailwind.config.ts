b0f8255a7f994b4279bc8a3842d53bb83a5a0864
ntsd <jo06942@gmail.com>
2024-07-21 23:10:08 +0700

    chore: change tailwind to typescript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import { light, dark } from "daisyui-ntsd";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,svelte,ts,svx,md}"],
@@ -8,12 +10,19 @@ module.exports = {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   darkMode: ["class"], // media for system dark mode
   daisyui: {
-    styled: true,
-    base: true,
-    utils: true,
-    logs: false,
-    rtl: false,
-    prefix: "",
-    themes: [...require("daisyui-ntsd")],
+    themes: [
+      {
+        light: {
+          ...light,
+          fontFamily: "Inter, sans-serif, system-ui",
+        },
+      },
+      {
+        dark: {
+          ...dark,
+          fontFamily: "Inter, sans-serif, system-ui",
+        },
+      },
+    ],
   },
 };
